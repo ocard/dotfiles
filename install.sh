8ad7226e076880eb460af134fab00dfa74fb7e5d
OC <7578529+ocard@users.noreply.github.com>
2018-12-30 14:49:02 -0300

    install st,yc and ls++

--- install.sh
+++ install.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 
+## shamelessly stolen from https://github.com/LukeSmithxyz/LARBS/blob/master/larbs.sh
+gitmakeinstall() {
+	dir=$(mktemp -d)
+	git clone --depth 1 "$1" "$dir" &>/dev/null
+	cd "$dir" || exit
+    case "$1" in
+	    "https://github.com/trapd00r/ls--.git")     
+            cpan Term::ExtendedColor
+            perl Makefile.PL 
+            ;;
+        *)
+            ;;
+	esac
+	make &>/dev/null
+	make install &>/dev/null
+    case "$1" in
+	    "https://github.com/trapd00r/ls--.git")     
+            cp ls++.conf $HOME/.ls++.conf
+            ;;
+        *)
+            ;;
+	esac
+	cd /tmp || return ;}
+
+
+gitmakeinstall(https://github.com/ocard/st)
+gitmakeinstall(https://github.com/ocard/yeahconsole)
+gitmakeinstall(https://github.com/trapd00r/ls--.git)
+
 ln -sv ~/Dotfiles/.zshrc                            ~ 
 ln -sv ~/Dotfiles/.tmux.conf                        ~
 
@@ -9,5 +38,3 @@ ln -sv ~/Dotfiles/global_gitignore                  ~/.config/git/ignore
 
 # make a link to crontab file in this folder
 ln -sv /etc/crontab                                 ~/Dotfiles
-
-
